bcf6635df8c709dcf7adef2642e1e90bab7055e3
unknown <dsever776@gmail.com>
2023-07-30 17:10:40 +0200

    Update deploy.sh and docker-compose.yaml
    Signed-off-by: unknown <dsever776@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ service2_version=""
 function usage {
     echo "Usage: $0 [--service1_version=VERSION] [--service2_version=VERSION]"
     echo ""
-    echo "Example: ./deploy --service1_version=v1.0 --service2_version=v1.0"
+    echo "Example: ./deploy.sh --service1_version=v1.0 --service2_version=v1.0"
     exit 1
 }
 
@@ -29,7 +29,17 @@ if [[ -z "$service1_version" || -z "$service2_version" ]]; then
     usage
 fi
 
+echo "Building images and running containers with following versions: "
 echo "Service 1 version: $service1_version"
 echo "Service 2 version: $service2_version"
 
 
+CMD="TAG1=$service1_version TAG2=$service2_version docker-compose up -d"
+
+eval $CMD
+
+echo "TESTING SERVICES WITH CURL: "
+
+sleep 3
+
+curl -X POST -H "Content-Type: application/json" -d '{"url":"https://www.index.hr/"}' http://localhost:8001/hash_webpage
